74bbcf8298f93fb1ccbaf457fca4d26844477f57
Тиханович Кирилл Сергеевич <kiryl.tsikhanovich@softclub.by>
2026-03-06 14:54:37 +0300

    Add ssr

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import ssr from "vite-plugin-ssr/plugin";
 
 export default defineConfig({
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
+    ssr(),
     tailwindcss(),
   ],
   base: "/",
