7103e52dc0ece5a7b4b55168af8e3df5660b3456
Тиханович Кирилл Сергеевич <kiryl.tsikhanovich@softclub.by>
2026-03-06 15:53:39 +0300

    Add meta tags remove ssr, prerender

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from "vite";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { vitePrerenderPlugin } from "vite-prerender-plugin";
 
 export default defineConfig({
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
-    vitePrerenderPlugin(),
     react(),
     tailwindcss(),
   ],
